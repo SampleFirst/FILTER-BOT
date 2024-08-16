a2252e6a9ef237d42c6f77cdde1d4f97292bae0c
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-08-16 19:13:05 +0530

    Update commands.py8

--- plugins/commands.py
+++ plugins/commands.py
@@ -4,7 +4,7 @@
 
 import os, string, logging, random, asyncio, time, datetime, re, sys, json, base64
 from Script import script
-from pyrogram import Client, filters, enums
+from pyrogram import TechVJBot as Client, filters, enums
 from pyrogram.errors import ChatAdminRequired, FloodWait
 from pyrogram.types import *
 from database.ia_filterdb import Media, get_file_details, unpack_new_file_id, get_bad_files
